f57122743add6af5946a37e5e556c521ab8a8aed
suly <robinjackson@qq.com>
2020-07-10 16:52:09 +0800

    add comment

--- pool.go
+++ pool.go
@@ -1,5 +1,6 @@
 package pool
 
+// Pool represents a goroutine pool.
 type Pool struct {
 	idleTimeout int64
 	routinePool []*routine
@@ -7,6 +8,12 @@ type Pool struct {
 	termSignal  chan bool
 }
 
+// New return a new pool.
+// It accept two args, maxRoutine and taskQueueSize.
+// maxRoutine represents how many goroutine can be forked in the pool.
+// taskQueueSize represents the task queue size.
+// When all of the goroutines are working and more task is start,
+// the new task will cached in a taskQueue and waiting for the standby routine.
 func New( /*maxRoutine, taskQueueSize int*/ maxAndSize ...int) *Pool {
 	var maxRoutine, taskQueueSize int
 
@@ -28,6 +35,7 @@ inital:
 		taskQueue:   make(chan Runnable, taskQueueSize),
 	}
 
+	// initialize the routine pool slice
 	for i := 0; i < maxRoutine; i++ {
 		p.routinePool[i] = new(routine)
 	}
@@ -36,10 +44,14 @@ inital:
 	return p
 }
 
+// SetIdleTimeout will set the idle timeout of the routine in the pool.
+// SetIdleTimeout is ok to be called at anytime, but only affected to the newly forked routine.
 func (p *Pool) SetIdleTimeout(idleTimeout int64) {
 	p.idleTimeout = idleTimeout
 }
 
+// masterRoutine is the master routine of handling task.
+// it only called once when creating a pool in pool.New.
 func (p *Pool) masterRoutine() {
 	idleQueue := make(chan chan Runnable)
 	tryFork := make(chan bool, 1)
@@ -48,34 +60,49 @@ func (p *Pool) masterRoutine() {
 
 	for {
 		select {
+		// Firstly, master routine try to pop a task from taskQueue.
 		case task, ok := <-p.taskQueue:
 			if !ok {
+				// If taskQueue was closed, master routine will shutdown all the routines gracefully.
+				// In the for loop, we remove all the down routines until the routine pool is empty.
 				for p.removeDownRoutine(); 0 < len(p.routinePool); p.removeDownRoutine() {
 					select {
 					case taskCh := <-idleQueue:
+						// close the taskCh, let the routine return
+						// the routine status will be set to down
 						close(taskCh)
 					default:
 					}
 				}
 
+				// if all the routines shuteddown successfully, send the finish signal.
 				p.termSignal <- true
 				return
 			}
-			tryFork <- true
 
+			// Secondly, master routine will try to pop a taskCh,
+			// then push the task into the taskCh, let the routine handle it.
+			tryFork <- true
 		popRoutineLoop:
 			for {
 				select {
 				case taskCh := <-idleQueue:
-					taskCh <- task
-					<-tryFork
+					taskCh <- task // If taskCh select successful, just push task into the routine.
+					<-tryFork      // clear the tryFork for the next newly task
 					break popRoutineLoop
 
 				default:
+					// If no standby taskCh, we try to fork a new one.
+					// but fork should do only onetime
 					for {
 						select {
+						// Fork for first time, the second time will block
 						case <-tryFork:
 							p.fork(idleQueue)
+
+							// If new routine truly forked, just push the task into it
+							// But if the pool is full of working routine,
+							//there is nothing to do but wait.
 						case taskCh := <-idleQueue:
 							taskCh <- task
 
@@ -91,6 +118,8 @@ func (p *Pool) masterRoutine() {
 	}
 }
 
+// fork try to start a new routine,
+// it travel through the routinePool and try to find a routine with status down.
 func (p *Pool) fork(idleQueue chan<- chan Runnable) bool {
 	for _, routine := range p.routinePool {
 		if routineStatusDown == routine.status() {
@@ -103,10 +132,9 @@ func (p *Pool) fork(idleQueue chan<- chan Runnable) bool {
 	return false
 }
 
-func (p *Pool) allDown() bool {
-	return len(p.routinePool) == 0
-}
-
+// removeDownRoutine will remove the routine with down status
+// and decrease the routinePool size
+// it should only called when the pool is shuting down.
 func (p *Pool) removeDownRoutine() {
 	filter := p.routinePool[:0]
 
@@ -119,10 +147,22 @@ func (p *Pool) removeDownRoutine() {
 	p.routinePool = filter
 }
 
+// Start will push the Runnable task into the taskQueue
+// which means a routine will start and work.
+// A panic will occured if the pool is already shutdown, because of the taskQueue is closed.
+// be careful in concurrency scenario.
 func (p *Pool) Start(task Runnable) {
 	p.taskQueue <- task
 }
 
+// Shutdown will stop all the routine gracefully.
+// If routine is standby, it will shutdown.
+// If routine is working, master routine will wait for routine finish it work by itself
+// and turn into standby, then shut it down.
+
+// Shutdown will return a [chan bool].
+// A true value will push into the channel when all the routines are shuted down.
+// you can wait for the popped signal to ensure all the task was handled or just ignore it.
 func (p *Pool) Shutdown() chan bool {
 	p.termSignal = make(chan bool)
 	close(p.taskQueue)
--- routine.go
+++ routine.go
@@ -8,8 +8,23 @@ const (
 	routineStatusWorking = 2
 )
 
+// The routine type represents a goroutine.
+// Its value is a uint8, it will be the one of the const routineStatus.
+// A routine instance is bind with a goroutine (see method routine.run),
+// and the run func will change the routine value.
 type routine uint8
 
+// The run method works as the entrance of the goroutine.
+
+// Arg idleQueue is an unbuffered [chan<- chan Runnable] type.
+// after run started, it will push a taskCh[chan Runnable] into idleQueue
+// to notify the pool that the routine is ready for handling task.
+// taskCh is also unbuffered.
+// Once the he pool selected a taskCh, the routine will waiting for the task from taskCh.
+// A task is the type which is implements Runnable interface.
+
+// Arg idleTimeout represents the time to wait of the routine,
+// if idleTimeout is 0, the routine will never timeout.
 func (r *routine) run(idleQueue chan<- chan Runnable, idleTimeout int64) {
 	if 0 == idleTimeout {
 		r.foreverRun(idleQueue)
@@ -18,6 +33,7 @@ func (r *routine) run(idleQueue chan<- chan Runnable, idleTimeout int64) {
 	}
 }
 
+// foreverRun is the routine without timeout
 func (r *routine) foreverRun(idleQueue chan<- chan Runnable) {
 	defer r.setStatus(routineStatusDown)
 
@@ -28,10 +44,16 @@ func (r *routine) foreverRun(idleQueue chan<- chan Runnable) {
 		r.setStatus(routineStatusStandby)
 
 		select {
+		// An idleQueue is an unbuffered chan,
+		// it means the pushing of taskCh will be blocked.
+		// Once the taskCh push into the idleQueue,
+		// the routine is being selected to handle task.
 		case idleQueue <- taskCh:
 			select {
 			case task, ok := <-taskCh:
 				if !ok {
+					// If the taskCh is selected but closed,
+					// it means the pool is shuting down the routine manually.
 					return
 				}
 
@@ -42,18 +64,20 @@ func (r *routine) foreverRun(idleQueue chan<- chan Runnable) {
 	}
 }
 
+//expireRun is the routine with timeout
 func (r *routine) expireRun(idleQueue chan<- chan Runnable, idleTimeout int64) {
 	defer r.setStatus(routineStatusDown)
 
 	taskCh := make(chan Runnable)
 	idleDuration := time.Duration(idleTimeout) * time.Second
-	timer := time.NewTimer(idleDuration)
+	timer := time.NewTimer(idleDuration) // timer is created for timeout
 	defer timer.Stop()
 
 	for {
 		r.setStatus(routineStatusStandby)
 
 		select {
+		// if the timer timeout, we will stop the routine
 		case <-timer.C:
 			return
 
@@ -67,7 +91,11 @@ func (r *routine) expireRun(idleQueue chan<- chan Runnable, idleTimeout int64) {
 				r.setStatus(routineStatusWorking)
 				task.Run()
 
+				// timer may timeout during the task handling,
+				// no matter what, we reset the timer, let the routine goes on.
 				if !timer.Stop() {
+					// If the timer push the timeout time,
+					// it must be popped before the next loop to avoid the terminated of routine.
 					<-timer.C
 				}
 				timer.Reset(idleDuration)
@@ -76,14 +104,19 @@ func (r *routine) expireRun(idleQueue chan<- chan Runnable, idleTimeout int64) {
 	}
 }
 
+// setStatus is called in run,
+// it also called before the pool forking a routine.
 func (r *routine) setStatus(status uint8) {
 	*r = routine(status)
 }
 
+// status reports the status of the routine.
 func (r *routine) status() uint8 {
 	return uint8(*r)
 }
 
+// Runnable represents a task which will call in routine.
+// It is the contract of routine.
 type Runnable interface {
 	Run()
 }
